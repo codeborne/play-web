63a57e70d7b1b29a526f006541f28a3e5a76d595
Anton Keks <anton@codeborne.com>
2013-07-18 14:55:15 +0300

    reindex pages every 6 hours

--- app/jobs/WebPageIndexerJob.java
+++ app/jobs/WebPageIndexerJob.java
@@ -1,13 +1,14 @@
 package jobs;
 
 import play.db.jpa.NoTransaction;
+import play.jobs.Every;
 import play.jobs.Job;
 import play.jobs.OnApplicationStart;
 import util.WebPageIndexer;
 
 import javax.inject.Inject;
 
-@OnApplicationStart(async = true) @NoTransaction
+@OnApplicationStart(async = true) @Every("6h") @NoTransaction
 public class WebPageIndexerJob extends Job {
   @Inject static WebPageIndexer indexer;
 
