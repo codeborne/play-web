76f1fd0b544482c5ea0d06c0c4e36f17eee65b70
Anton Keks <anton@codeborne.com>
2013-07-31 16:12:52 +0300

    can now restore files

--- app/controllers/WebAdmin.java
+++ app/controllers/WebAdmin.java
@@ -99,6 +99,17 @@ public class WebAdmin extends BaseController {
     render(page, revision, diff);
   }
 
+  public static void restore(String path, String revision) throws InterruptedException, IOException, Git.ExecException {
+    WebPage page = WebPage.forPath(path);
+    List<String> args = new ArrayList<>(asList("checkout", revision, "--"));
+    if (path.startsWith("/")) path = path.substring(1);
+    for (VirtualFile file : page.dir.list()) {
+      if (!file.isDirectory()) args.add(path + file.getName());
+    }
+    Logger.info("Restored " + page.path + " to " + revision);
+    redirect(page.path);
+  }
+
   public static void doc() throws IOException {
     Collection<WebPage.Template> templates = WebPage.availableTemplates();
     render(templates);
