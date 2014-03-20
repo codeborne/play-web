ce90f3848947d6930e92646dbea189db82266a1f
Andrei Solntsev <andrei.solntsev@gmail.com>
2014-03-20 23:31:48 +0200

    Run unit-tests as a part of build

---
+++ mocked-sources-for-test/controllers/Security.java
@@ -0,0 +1,15 @@
+package controllers;
+
+public class Security {
+  public static boolean isConnected() {
+    return false;
+  }
+
+  public static boolean check(String role) {
+    return false;
+  }
+
+  public static String connected() {
+    return null;
+  }
+}
