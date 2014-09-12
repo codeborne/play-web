84f97da03e7303d2d0c11023f6063180815a8a92
Andrei Solntsev, Tarmo Ojala <andrei@codeborne.com, tarmo@codeborne.com>
2014-09-12 15:47:05 +0300

    removed deprecation warnings from new request creation, as the suggested replacement is awful

--- test/plugins/WebContentPluginTest.java
+++ test/plugins/WebContentPluginTest.java
@@ -31,8 +31,7 @@ public class WebContentPluginTest {
   public void setUp() {
     if (Play.configuration == null) Play.configuration = new Properties();
     if (Play.id == null) Play.id = "dev";
-    //noinspection deprecation
-    Http.Request.current.set(request = new Http.Request());
+    Http.Request.current.set(request = newHttpRequest());
     Http.Response.current.set(response = new Http.Response());
     Scope.RenderArgs.current.set(new Scope.RenderArgs());
     request.path = "/";
@@ -43,6 +42,11 @@ public class WebContentPluginTest {
     plugin = new WebContentPlugin();
     pages = asList(forPath("/en/marketing/"), forPath("/ru/marketing/"));
     resetWebCacheSetting();
+  }
+
+  private Http.Request newHttpRequest() {
+    @SuppressWarnings("deprecation") Http.Request result = new Http.Request();
+    return result;
   }
 
   @After
